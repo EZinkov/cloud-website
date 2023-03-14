cf5e0b27923dfe281b332af812fb45869692f423
Evgeniy <evgeniyzinkov98@gmail.com>
2023-03-14 14:02:01 +0100

    add: import components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import About from "./components/About"
+import AllInOne from "./components/AllInOne"
+import Footer from "./components/Footer"
+import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
-import "./index.css"
+import Pricing from "./components/Pricing"
+import Support from "./components/Support"
 
 function App() {
   return (
-    <div className="">
+    <>
       <Navbar />
-    </div>
+      <Hero />
+      <About />
+      <Support />
+      <AllInOne />
+      <Pricing />
+      <Footer />
+    </>
   )
 }
 
